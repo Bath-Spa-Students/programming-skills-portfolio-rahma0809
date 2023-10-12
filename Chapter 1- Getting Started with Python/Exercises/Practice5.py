1d0b9f706e9924540dac5b22e444797fd3104b5e
Rahma <rahmareyaz91@gmail.com>
2023-10-12 09:56:29 +0400

    practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice5.py
@@ -0,0 +1,6 @@
+# Write four strings in different variables and print the output as one string. (string concatenation)
+firstname='Rahma'
+lastname=' Reyaz'
+university=' Bathspa'
+room=' F05'
+print(firstname+lastname+university+room)
