fb1d1949d0ebe9a9ec07cdc69f83d6accab58b7b
Rahma <rahmareyaz91@gmail.com>
2023-10-16 09:29:27 +0400

    exercise5.py

--- Chapter 2- Variables & Comments/Exercises/Excercise4.py
+++ Chapter 2- Variables & Comments/Exercises/Excercise4.py
@@ -1,3 +1,7 @@
-fav_number= 8
-print(f'My fav number is:{fav_number}')
+price= 8
+total_money= 50
+num=total_money // price
+money_left= total_money % price
+print(f'number of USB sticks she can buy::{num}')
+print(f'money left after buying USB sticks{money_left}')
 
---
+++ Chapter 2- Variables & Comments/Exercises/Excercise5.py
@@ -0,0 +1,6 @@
+price= 8
+total_money= 50
+num=total_money // price
+money_left= total_money % price
+print(f'number of USB sticks she can buy::{num}')
+print(f'money left after buying USB sticks{money_left}')
