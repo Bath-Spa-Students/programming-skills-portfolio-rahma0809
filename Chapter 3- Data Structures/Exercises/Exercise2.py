0f80b6fc7206faeebc96218c597d3be433874a8f
Rahma <rahmareyaz91@gmail.com>
2023-10-22 18:02:47 +0400

    exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,11 @@
+Names=['Sadaf','Aliya','Ammara','Angel','Liya','Shaima','Asfia','Dania','Aisha','Zeenat']
+print('Hello',Names [0],'How have you been ?')
+print('Hello',Names [1],'How have you been ?')
+print('Hello',Names [2],'How have you been ?')
+print('Hello',Names [3],'How have you been ?')
+print('Hello',Names [4],'How have you been ?')
+print('Hello',Names [5],'How have you been ?')
+print('Hello',Names [6],'How have you been ?')
+print('Hello',Names [7],'How have you been ?')
+print('Hello',Names [8],'How have you been ?')
+print('Hello',Names [9],'How have you been ?')
