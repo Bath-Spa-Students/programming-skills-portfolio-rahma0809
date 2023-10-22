0310bcbaa7178fdf8296424dc3e17233826322d7
Rahma <rahmareyaz91@gmail.com>
2023-10-22 20:29:11 +0400

    exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,4 @@
+invitation=['Ms.Sadaf','Ms.Steena','Taniya']
+print(f'Greetings!,',invitation[0],'You are invited for a dinner party at the Benz hotel on Oct 29 at 8:30 pm. Looking forward to meet you')
+print(f'Good Morning!,',invitation[1],'Why dont we get together at the Benz hotel on Oct 29 at 8:30 pm? We all will have lots of fun')
+print(f'Heyy!,',invitation[2],'Let us meet for a dinner party at the Benz hotel on Oct 29 at 8:30 pm. It was long week let us take some break and enjoy our day')
