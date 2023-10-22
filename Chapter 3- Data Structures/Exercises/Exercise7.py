4a06d9834f198d663544eae43f6951ca79a80dd7
Rahma <rahmareyaz91@gmail.com>
2023-10-22 22:36:19 +0400

    exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,12 @@
+Places=['Maldives','New York','Turkey','China','Tokoyo']
+print(Places)
+print(sorted(Places))
+print(Places)
+print(sorted(Places,reverse=True))
+print(Places)
+Places.reverse
+print(Places)
+Places.sort
+print(Places)
+Places.sort(reverse=True)
+print(Places)
