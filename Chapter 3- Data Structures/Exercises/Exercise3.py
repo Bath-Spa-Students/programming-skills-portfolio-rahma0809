7488265613140e301ec97a0e31f87e252f2c3069
Rahma <rahmareyaz91@gmail.com>
2023-10-22 20:27:19 +0400

    exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+fav_mode_transport=['Aeroplane','Car','Bike','Jet','Cruise','Metro']
+print('we travel to different countries through',fav_mode_transport[0])
+print('long',fav_mode_transport[1],'drive is always the best')
+print('I would like to learn how to drive a',fav_mode_transport[2])
+print('celebrities usually own a private',fav_mode_transport[3])
+print('Parties in a',fav_mode_transport[4], 'are amazing')
+print('It is easier to use',fav_mode_transport[5])
