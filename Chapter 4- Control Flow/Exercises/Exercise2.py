e8f09b956e439727e09e18006a7be05bc23a5cbc
Rahma <rahmareyaz91@gmail.com>
2023-10-31 00:46:08 +0400

    Exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,13 @@
+#if block
+alien_color='Green'
+if alien_color=='Green':
+    print('Congratulations!, You just earned 5 points for shooting the alien')
+else:
+    print('Congratulations!, You just earned 10 ponts for shooting the alien')
+
+#else block
+alien_color='Red'
+if alien_color=='Green':
+    print('Congratulations!, You just earned 5 points for shooting the alien')
+else:
+    print('Congratulations!, You just earned 10 ponts for shooting the alien')
