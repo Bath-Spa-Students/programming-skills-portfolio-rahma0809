85155c3704cb1210dd3a9a619dbb451d3db6b242
Rahma <rahmareyaz91@gmail.com>
2023-10-31 00:55:46 +0400

    Exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+alien_color='Red'
+if alien_color=='Green':
+    print('Congratulations!, You just earned 5 points')
+elif alien_color=='Yellow':
+    print('Congratulations!, You just earned 10 points')
+else:
+    print('Congratulations!, You just earned 15 points ')
