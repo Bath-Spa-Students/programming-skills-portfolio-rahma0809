a162618cc227f036953fcc414e148b9a067b385a
Rahma <rahmareyaz91@gmail.com>
2023-11-01 01:10:35 +0400

    Exercise5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,11 @@
+favoriate_fruits=['Watermelons','Cherries','Litchi']
+if 'Watermelons' in favoriate_fruits:
+    print('You really like Watermelons')
+if 'Cherries' in favoriate_fruits:
+    print('You really like Cherries')
+if 'Mangoes' in favoriate_fruits:
+    print('You really like Mangoes')
+if 'Oranges' in favoriate_fruits:
+    print('You really like Oranges')
+if 'Peaches' in favoriate_fruits:
+    print('You really like Peaches')
