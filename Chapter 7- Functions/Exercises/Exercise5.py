a1497da39abd780cc0d6f1a983869a33861a172d
Rahma <rahmareyaz91@gmail.com>
2023-11-08 02:25:24 +0400

    exercise5.py

---
+++ Chapter 7- Functions/Exercises/Exercise5.py
@@ -0,0 +1,8 @@
+def describe_city(city, country=" Unknown"):
+    information =(city+ 'is in'+ country)
+    print(information)
+
+# function for different cities and countries
+describe_city("Reykjavik", " Iceland")
+describe_city("Abudhabi", " UAE")
+describe_city("Germany")
