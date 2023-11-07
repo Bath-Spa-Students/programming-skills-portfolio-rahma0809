1d15ae0d8756e35add6b932078c8a35376cdd504
Rahma <rahmareyaz91@gmail.com>
2023-11-08 01:48:41 +0400

    exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,5 @@
+def make_shirt(size,message):
+    summary =('\n''Creating a'+ size +'-sized shirt with the message:'+message)
+    print(summary)
+make_shirt(' Small',' Keep smiling')
+make_shirt(size=" Medium",message= " sunshine")
