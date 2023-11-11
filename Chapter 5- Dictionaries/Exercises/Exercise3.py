629feb7d61b4f70180c77b0d6d3e826a0562ab0f
Rahma <rahmareyaz91@gmail.com>
2023-11-12 00:48:03 +0400

    exercise3.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -0,0 +1,12 @@
+Glossary={'type':'a built-in function that is used to return the type of data stored in the objects or variables in the program.',
+          'int':'a whole number, positive or negative, without decimals',
+          'dictionary':'A collection of key-value pairs,',
+          'string':'A series of characters',
+          'input':'request user input',
+          'key': 'The first item in a key-value pair in a dictionary',
+          'value': 'An item associated with a key in a dictionary',
+          'conditional test': 'A comparison between two values',
+          'float': 'A numerical value with a decimal component',
+          'boolean expression': 'An expression that evaluates to True or False',}
+for word, definition in Glossary.items():
+    print("\n" + word.title() + ": " + definition)
