59ca769a7afdfa08ddb928f3b175d28ae4ac7b77
Rahma <rahmareyaz91@gmail.com>
2023-11-13 10:55:15 +0400

    practice2.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice2.py
@@ -0,0 +1,9 @@
+cl1=int(input('Enter the marks of codelab 1:'))
+dvd=int(input('Enter the marks of dvd:'))
+dst=int(input('Enter the marks of dst:'))
+sum=cl1+dvd+dst
+avg=sum/3
+print(avg)
+total=int(input('Enter the total marks :'))
+per=sum/total*100
+print(per)
