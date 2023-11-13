b680e757115d1b7e790bc9dbe2c4ae415d8b6040
Rahma <rahmareyaz91@gmail.com>
2023-11-13 12:12:38 +0400

    practice3.py

---
+++ Chapter 3- Data Structures/Exercises/practice3.py
@@ -0,0 +1,4 @@
+#Assume names references a list. Write a for loop that displays each element of the list
+name=['Einstein', 'Newton' , 'Copernicus','Kepler']
+for names in name:
+    print(names)
---
+++ Chapter 3- Data Structures/Exercises/practice4.py
@@ -0,0 +1,7 @@
+'''Assume the lists numbers1 has 100 elements and numbers2 is an empty list. Write code that
+copies the values in numbers to numbers2.'''
+numbers1=[1]*100
+numbers2=[]
+numbers2=numbers1[:]
+print('numbers1:',numbers1)
+print('numbers2:',numbers2)
