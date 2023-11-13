69794ca47b735151d07c6f1ff644803c6743d7f7
Rahma <rahmareyaz91@gmail.com>
2023-11-14 00:19:14 +0400

    practice1.py

---
+++ Chapter 7- Functions/Exercises/practice1.py
@@ -0,0 +1,10 @@
+#Write a Python function that takes two numbers as arguments and returns their sum
+def add_numbers(a, b):
+    return a + b
+a = 7
+b=5
+result = add_numbers(a,b)
+
+print(f"The sum of {a} and {b} is: {result}")
+
+
