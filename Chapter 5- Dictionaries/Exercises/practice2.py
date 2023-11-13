56b7dfd3cf4b6b32254e1c8f8c0d42a5ab0751c1
Rahma <rahmareyaz91@gmail.com>
2023-11-13 13:21:57 +0400

    practice2.py

---
+++ Chapter 5- Dictionaries/Exercises/practice2.py
@@ -0,0 +1,10 @@
+#Write a Python program to iterate through the keys of a dictionary and print them.
+
+dict={'Name':'Rahma',
+      'Age':18,
+      'year':'one',
+      'hobby':'reading',
+      'height':'158cm'}  
+print(dict)
+for key in dict:
+    print(key)
