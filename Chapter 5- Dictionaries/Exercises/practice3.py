a88bf367f5588cc3cbf7053a968a2c58b423ba81
Rahma <rahmareyaz91@gmail.com>
2023-11-13 13:25:53 +0400

    practice3.py

---
+++ Chapter 5- Dictionaries/Exercises/practice3.py
@@ -0,0 +1,9 @@
+#Write a Python program to iterate through the values of a dictionary and print them.
+dict={'Name':'Rahma',
+      'Age':18,
+      'year':'one',
+      'hobby':'reading',
+      'height':'158cm'}  
+print(dict)
+for value in dict.values():
+    print(value)
