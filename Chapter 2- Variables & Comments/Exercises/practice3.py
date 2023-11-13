6f2a5632c6804de48953d9830a157b57fc05d3f1
Rahma <rahmareyaz91@gmail.com>
2023-11-13 11:33:26 +0400

    practice1.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice3.py
@@ -0,0 +1,10 @@
+'''Write a python program that takes an input 5 from user and then type cast those values to string, int
+and float in the separate variables. Print all the variables.'''
+a=input('Enter your name:')
+b=input('Enter your age:')
+c=input('Enter your weight:')
+print(str(a))
+print(int(b))
+print(float(c))
+
+
---
+++ Chapter 2- Variables & Comments/Exercises/practice4.py
@@ -0,0 +1,6 @@
+'''Write a python program that stores an integer and string value to variables x and y. Print the type of
+variable x and y. '''
+x=150
+y='Rahma'
+print(type(x))
+print(type(y))
